04bc9385b61f76ca9a1cd3157a40ebe925dbfc0d
woo2851 <82019092+woo2851@users.noreply.github.com>
2021-06-14 16:06:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -96,3 +96,46 @@ const contactMe = document.querySelector('#contact')
 contactBtn.addEventListener('click' , () => {
   contactMe.scrollIntoView({behavior: 'smooth'})
 })
+
+// navbar borer active
+
+const sectionIds = ['#home','#about','#skills','#work','#testimonials','#contact' ]
+
+const sections = sectionIds.map(id => document.querySelector(id))
+const navItems = sectionIds.map(id => document.querySelector(`[data-link ='${id}']`))
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.7
+}
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      let sectionIndex = sections.indexOf(entry.target)
+      let sectionMinus = sectionIndex - 1
+      let sectionPlus = sectionIndex + 1
+
+      navItems[sectionIndex].classList.add('active')
+      if(sectionIndex === 0){
+        if(navItems[sectionPlus].matches('.active')){
+          navItems[sectionPlus].classList.remove('active')
+        }
+      }
+      else if(sectionIndex >= 0 && 4 >= sectionIndex){
+        navItems[sectionMinus].classList.remove('active')
+        if(navItems[sectionPlus].matches('.active')){
+          navItems[sectionPlus].classList.remove('active')
+          navItems[sectionIndex].classList.add('active')
+        }
+      }
+      else if (sectionIndex === 5){
+        navItems[sectionMinus].classList.remove('active')
+        navItems[sectionIndex].classList.add('active')
+      }}
+})}
+
+const observer = new IntersectionObserver(callback, observerOptions)
+sections.forEach(section => observer.observe(section))
+
